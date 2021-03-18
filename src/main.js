637ede6c213cf286585831a91847f3c78d9ee815
Jessie Hung <jessiehung@onead.com.tw>
2021-03-18 23:47:58 +0800

    Router Setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Router from 'vue-router'
+import BookList from './book_list.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/books',
+      component: BookList,
+      meta: {},
+      children: []
+    }
+  ]
+});
+
 new Vue({
-  render: h => h(App),
+  router: router,
+  render: h => h(BookList),
 }).$mount('#app')
